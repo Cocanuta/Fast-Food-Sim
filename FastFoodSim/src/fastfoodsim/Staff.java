b09598c564dafd2878d52293031453a1f2493fe4
Engolus <itsdavidengles@gmail.com>
2014-01-13 14:47:28 +0000

    Initial Classes and Variables
    Initial classes (customers, staff, restaurant and skills) with variables
    and TODO list.

---
+++ FastFoodSim/src/fastfoodsim/Staff.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fastfoodsim;
+
+/**
+ *
+ * @author Engolus
+ */
+public class Staff {
+    
+    // **** CLASS VARIABLES **** //
+    
+    private String staffName;               // The name of the staff member.
+    private Appearance staffAppearance;     // What the staff member looks like.
+    private float staffWage;                // The wage the staff member is paid.
+    private Skill staffSkills;              // The skills the staff member has.
+    private String staffRole;               // The role of the staff member.
+    private Availability staffAvailability; // The availability of the staff member.
+    private float staffSatisfaction;        // How satisfied the staff member is.
+    
+}
